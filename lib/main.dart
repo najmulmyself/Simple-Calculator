1f8ebe4b85c5a58305c4c1abb88ad6ee8d3d7ba8
najmulmyself <najmul.myself@gmail.com>
2022-06-11 15:52:28 +0600

    added math expression and replace sign

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unnecessary_const
 
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,10 +38,35 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
       if (buttonText == 'C') {
         equation = '0';
         result = '0';
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
       } else if (buttonText == '⌫') {
+        equationFontSize = 48.0;
+        resultFontSize = 38.0;
         equation = equation.substring(0, equation.length - 1);
+        if (equation == '') {
+          equation = '0';
+          equationFontSize = 38.0;
+          resultFontSize = 48.0;
+        }
       } else if (buttonText == '=') {
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+
+        expression = equation;
+        expression = expression.replaceAll('×', '*');
+        expression = expression.replaceAll('÷', '/');
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        } catch (e) {
+          result = 'Error';
+        }
       } else {
+        equationFontSize = 48.0;
+        resultFontSize = 38.0;
         if (equation == '0') {
           equation = buttonText;
         } else {
